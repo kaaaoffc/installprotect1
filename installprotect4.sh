c434a89af22fafca0dbc2fd5c8b715f6bd66711c
KAAA HOSTING <kaaahosting@gmail.com>
2026-01-04 19:33:51 +0700

    Update installprotect4.sh

--- installprotect4.sh
+++ installprotect4.sh
@@ -44,7 +44,7 @@ class NodeController extends Controller
         // === 🔒 FITUR TAMBAHAN: Anti akses selain admin ID 1 ===
         $user = Auth::user();
         if (!$user || $user->id !== 1) {
-            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nodes. 𝗣𝗥𝗢𝗧𝗘𝗖𝗧 𝗕𝗬 @𝗸𝗮𝗮𝗮𝗵𝗼𝘀𝘁𝗶𝗻𝗴');
+            abort(403, '🚫 Akses ditolak! Hanya admin ID 1 yang dapat membuka menu Nodes. 𝗣𝗥𝗢𝗧𝗘𝗖𝗧 𝗕𝗬 @kaaahost1');
         }
         // ======================================================
 
